05a77f099ecad838f7b86d42d7b5d5918ce40d7d
giuliamagrin <150248459+giuliamagrin@users.noreply.github.com>
2024-01-15 14:31:15 +0100

    Create full coverage unit tests

--- backend/app/mymodules/filter_functions.py
+++ backend/app/mymodules/filter_functions.py
@@ -1,23 +1,6 @@
 import pandas as pd
 
 
-def filter_dataframe(df, column, value):
-      """
-     Filter a DataFrame based on a specific value in a specified column.
-
-     Parameters:
-         df (pd.DataFrame): The DataFrame to be filtered.
-         column (str): The name of the column in the DataFrame for filtering.
-         value: The specific value to filter for in the specified column.
-
-     Returns:
-         pd.DataFrame: Filtered DataFrame containing
-                      only rows where the specified
-                      column has the specified value.
-     """
-      return df[df[column] == value]
-
-
 def filter_contains(df, column, value):
      """
      Filter a DataFrame based on substring matching in a specified column.
@@ -31,14 +14,11 @@ def filter_contains(df, column, value):
          pd.DataFrame: Filtered DataFrame containing
                        only rows that match the substring condition.
      """
-     if column == 'year':
-          return df[df["year"] == str(value)]  
-     else:
-          return df[df[column].str.contains(value, case=False, na=False)]
+     return df[df[column].str.contains(value, case=False, na=False)]
 
 
 def filter_range(df, column, min_value, max_value):
-      """
+     """
      Filter a DataFrame based on a numeric range or exact
      match for the 'year' column.
 
@@ -52,5 +32,5 @@ def filter_range(df, column, min_value, max_value):
      Returns:
          pd.DataFrame: Filtered DataFrame containing only rows where the
          specified column's values fall within the specified range.
-    """
-      return df[(df[column] >= min_value) & (df[column] <= max_value)]
+     """
+     return df[(df[column] >= min_value) & (df[column] <= max_value)]
